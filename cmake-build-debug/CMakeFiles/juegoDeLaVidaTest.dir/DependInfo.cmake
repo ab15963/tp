e52c39c986409d9db8b0bcabab10787deccba2b6
Andrés <a@a.com>
2018-11-03 00:46:21 -0300

    Son los archivos descargados. No agregué nada todavía.

---
+++ cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/juegoDeLaVida/main.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/main.obj"
+  "C:/juegoDeLaVida/solucion.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/solucion.obj"
+  "C:/juegoDeLaVida/tests/densidadPoblacionTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/densidadPoblacionTEST.obj"
+  "C:/juegoDeLaVida/tests/enCrecimientoTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/enCrecimientoTEST.obj"
+  "C:/juegoDeLaVida/tests/esPeriodicoTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/esPeriodicoTEST.obj"
+  "C:/juegoDeLaVida/tests/esValidoTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/esValidoTEST.obj"
+  "C:/juegoDeLaVida/tests/evolucionDePosicionTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionDePosicionTEST.obj"
+  "C:/juegoDeLaVida/tests/evolucionMultipleTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionMultipleTEST.obj"
+  "C:/juegoDeLaVida/tests/evolucionToroideTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionToroideTEST.obj"
+  "C:/juegoDeLaVida/tests/fusionarTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/fusionarTEST.obj"
+  "C:/juegoDeLaVida/tests/posicionesVivasTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/posicionesVivasTEST.obj"
+  "C:/juegoDeLaVida/tests/primosLejanosTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/primosLejanosTEST.obj"
+  "C:/juegoDeLaVida/tests/seleccionNaturalTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/seleccionNaturalTEST.obj"
+  "C:/juegoDeLaVida/tests/vistaTrasladadaTEST.cpp" "C:/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/vistaTrasladadaTEST.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/googletest-master/googletest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/juegoDeLaVida/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "C:/juegoDeLaVida/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/juegoDeLaVidaTest.dir/main.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/solucion.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/esValidoTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/posicionesVivasTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/densidadPoblacionTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionDePosicionTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionMultipleTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionToroideTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/esPeriodicoTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/primosLejanosTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/seleccionNaturalTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/fusionarTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/vistaTrasladadaTEST.obj"
+  "CMakeFiles/juegoDeLaVidaTest.dir/tests/enCrecimientoTEST.obj"
+  "../juegoDeLaVidaTest.pdb"
+  "../juegoDeLaVidaTest.exe"
+  "../juegoDeLaVidaTest.exe.manifest"
+  "../libjuegoDeLaVidaTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/juegoDeLaVidaTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
